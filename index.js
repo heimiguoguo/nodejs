9b65b6b7bbe0bc52da418bf77d986a754a083eba
heimiguoguo <mang.huang@hexagon.com>
2019-03-21 10:09:29 +0800

    重构项目

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-/**
- * Created by Tom on 2018/6/9.
- */
-var server = require("./server")
-var router = require("./router")
+const express = require('express');
+const app = express();// express.static 提供静态文件，就是html, css, js 文件
+app.use(express.static('public'));
 
+// 电话号码返回省和市，为了模拟延迟，使用了setTimeout
+app.post('/phoneLocation', (req, res) => {
+    setTimeout(() => {
+        res.json({
+            success: true,
+            obj: {
+                province: '广东',
+                city: '深圳'
+            }
+        })
+    }, 1000);
+})
 
-server.start(router.route)
+// 返回面值列表
+app.post('/faceList', (req, res) => {
+    setTimeout(() => {
+        res.json(
+            {
+                success: true,
+                obj:['20元', '30元', '50元', '90元']
+            }
+            
+        )
+    }, 1000);
+})
+
+app.listen(3000, () => {
+    console.log('server start');
+})
